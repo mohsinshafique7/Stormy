53c75ec0e638a963784b63d72b1d8f4303c84120
Mohsin <mohsin.shafique1991@gmail.com>
2020-04-22 21:33:29 +0500

    Initial commit

---
+++ app/src/main/java/com/example/stormy/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.stormy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String address = "asdasd";
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(address)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                try {
+                    if (response.isSuccessful())
+                        Log.e(TAG,  response.body().string());
+
+                } catch (IOException e) {
+                    Log.e(TAG,"Response Result",e);
+                }
+            }
+        });
+
+    }
+}
